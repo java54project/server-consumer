93faa8fe48f533460efe2dde1e61aab1bb52a561
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-12-28 13:17:36 +0200

    adds checking of database for duplicate antries and adds 3 retries to save data to database

--- services/dataServise.mjs
+++ services/dataServise.mjs
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk';
 
-// AWS config
+// Configure connection to DynamoDB
 AWS.config.update({
     region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -10,19 +10,51 @@ AWS.config.update({
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.DYNAMODB_TABLE_NAME;
 
-// saving data to DynamoDB
+// Check for duplicate entry
+export const isDuplicate = async (id) => {
+    const params = {
+        TableName: tableName,
+        Key: { id },
+    };
+    const result = await dynamoDB.get(params).promise();
+    return !!result.Item; // Returns true if the entry exists
+};
+
+// Save data with retry logic
+export const saveWithRetry = async (params, maxRetries = 3) => {
+    for (let i = 0; i < maxRetries; i++) {
+        try {
+            await dynamoDB.put(params).promise();
+            return; // Successful save, exit the loop
+        } catch (error) {
+            if (i === maxRetries - 1) {
+                throw error; // All attempts exhausted, throw the error
+            }
+        }
+    }
+};
+
+// Main function to save data to DynamoDB
 export const saveDataToDatabase = async (data) => {
+    const id = data.metadata.Board + '_' + new Date().toISOString();
+
+    // Check for duplicates
+    if (await isDuplicate(id)) {
+        throw new Error(`Duplicate entry found with id: ${id}`);
+    }
+
+    // Parameters for saving
     const params = {
         TableName: tableName,
         Item: {
-            id: data.metadata.Board + '_' + new Date().toISOString(), // uniq ID of the 
+            id,
             metadata: data.metadata,
             moves: data.moves,
             createdAt: new Date().toISOString(),
         },
     };
 
-    // pushing data to DynamoDB
-    await dynamoDB.put(params).promise();
+    // Save data with retry logic
+    await saveWithRetry(params);
     console.log('Data successfully saved to DynamoDB:', params.Item);
 };
